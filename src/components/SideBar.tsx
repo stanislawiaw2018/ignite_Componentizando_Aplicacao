f6f8738c70b34e7275125be3dd36bcf6d7eadee4
Antonio Stanislaw <stanislaw.aiw2018@gmail.com>
2023-02-09 23:34:02 -0300

    add project DESAFIO_2

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,48 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  handleClickButton: (id: number) => void;
+  selectedGenreld: number;
+
+
+}
+
+export function SideBar( {  handleClickButton,  selectedGenreld  } :  SidebarProps) {
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+
+    <nav className="sidebar">
+    <span>My<p>Movies</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreld === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+
+  )
 }
